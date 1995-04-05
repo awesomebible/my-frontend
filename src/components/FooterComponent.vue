14cd0451da979ec1362309da84b4aeef237940da
Benjamin Hartmann <42138517+benjaminwolkchen@users.noreply.github.com>
1995-04-05 17:36:51 +0200

    Update Copyright year according to the sytem clock

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -13,12 +13,21 @@
                 </router-link>
             </div>
             <div class="px-5 py-2">
-                <a href="https://codeberg.org/awesomebible/my-frontend" class="text-base leading-6 text-black-500 hover:text-black-900">
+                <a href="https://codeberg.org/awesomebible/my-united" class="text-base leading-6 text-black-500 hover:text-black-900">
                     Codeberg ⛰
                 </a>
             </div>
         </nav>
-        <p class="mt-8 text-base leading-6 text-center text-black-400" id="">© 2021 awesomeBible</p>
+        <p class="mt-8 text-base leading-6 text-center text-black-400">© {{date}} awesomeBible</p>
     </div>
 </footer>
 </template>
+<script>
+export default {
+    data() {
+        return {
+        date: new Date().getFullYear(),
+        };
+  },
+}
+</script>
