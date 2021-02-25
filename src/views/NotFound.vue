6391323f334a0434764d87864f0b517a504b0682
Benjamin Hartmann <42138517+benjaminwolkchen@users.noreply.github.com>
2021-02-25 13:13:19 +0100

    tails -> my.awesomeBible

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -4,7 +4,7 @@
         <nav class="flex items-center w-full h-24" x-data="{ showMenu: false }">
             <div class="relative flex flex-wrap items-center justify-between w-full h-24 mx-auto font-medium md:justify-center">
                 <a href="#_" class="w-1/4 py-4 pl-6 pr-4 md:pl-4 md:py-0">
-                    <span class="text-xl font-black leading-none text-white select-none logo">tails<span class="text-pink-500">.</span></span>
+                    <span class="text-xl font-black leading-none text-white select-none logo">my.awesomeBible<span class="text-pink-500">.</span></span>
                 </a>
                 <div class="fixed top-0 left-0 z-40 items-center hidden w-full h-full p-3 text-xl bg-gray-900 bg-opacity-50 md:text-sm lg:text-base md:w-3/4 md:bg-transparent md:p-0 md:relative md:flex" :class="{'flex': showMenu, 'hidden': !showMenu }">
                     <div class="flex-col w-full h-auto overflow-hidden bg-white rounded-lg select-none md:bg-transparent md:rounded-none md:relative md:flex md:flex-row md:overflow-auto">
