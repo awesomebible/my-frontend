fbcb3df24b9564e905436477179c9c5fd4433425
Benjamin Hartmann <42138517+benjaminwolkchen@users.noreply.github.com>
2021-03-12 19:47:43 +0100

    Remove empty line

--- src/views/login/Purchase.vue
+++ src/views/login/Purchase.vue
@@ -22,7 +22,6 @@
           Mit fortfahren, stimmst du auch dessen <a href="https://stripe.com/de/privacy">Datenschutzerkärung</a> zu.</p>
         </div>
       </div>
-
       <div class="flex items-center justify-between">
       </div>
         <button class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
