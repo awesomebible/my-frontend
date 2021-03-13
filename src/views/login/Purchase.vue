31b0acd7bdf572e53f6fe1e71414341408cbd5a6
Benjamin Hartmann <42138517+benjaminwolkchen@users.noreply.github.com>
2021-03-13 17:39:34 +0100

    Proper Spacing

--- src/views/login/Purchase.vue
+++ src/views/login/Purchase.vue
@@ -20,13 +20,11 @@
           <label for="invite-code" class="sr-only">Einladungscode</label>
           <input id="invite-code" name="invite-code" type="text" autocomplete="text" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Einladungscode">
         </div>
-        <div>
+        <div class="py-2 px-4">
           <p class="mt-2 text-center text-sm text-gray-600">Mit fortfahren stimmst du der my.awesomeBible AGB und Datenschutzerkärung zu.</p>
           <p class="mt-2 text-center text-sm text-gray-600">Wir benutzen zum verarbeiten der Zahlung den Online-Dienst <a href="https://stripe.com" class="font-medium text-indigo-600 hover:text-indigo-500">Stripe</a>.
           Mit fortfahren, stimmst du auch dessen <a href="https://stripe.com/de/privacy">Datenschutzerkärung</a> zu.</p>
         </div>
-      </div>
-      <div class="flex items-center justify-between">
       </div>
         <button class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           Weiter mit Stripe
