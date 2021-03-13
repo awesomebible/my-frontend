1d254b9990b676c3aa589ae3492138fd41e37b07
Benjamin Hartmann <42138517+benjaminwolkchen@users.noreply.github.com>
2021-03-13 14:06:48 +0100

    Update 404 a to router-link because it decreases network payload.

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -3,16 +3,16 @@
     <div class="mx-auto max-w-7xl">
         <nav class="flex items-center w-full h-24" x-data="{ showMenu: false }">
             <div class="relative flex flex-wrap items-center justify-between w-full h-24 mx-auto font-medium md:justify-center">
-                <a href="/" class="w-1/4 py-4 pl-6 pr-4 md:pl-4 md:py-0">
+                <router-link to="/" class="w-1/4 py-4 pl-6 pr-4 md:pl-4 md:py-0">
                     <span class="text-xl font-black leading-none text-white select-none logo">my.awesomeBible<span class="text-pink-500">.</span></span>
-                </a>
+                </router-link>
                 <div class="fixed top-0 left-0 z-40 items-center hidden w-full h-full p-3 text-xl bg-gray-900 bg-opacity-50 md:text-sm lg:text-base md:w-3/4 md:bg-transparent md:p-0 md:relative md:flex" :class="{'flex': showMenu, 'hidden': !showMenu }">
                     <div class="flex-col w-full h-auto overflow-hidden bg-white rounded-lg select-none md:bg-transparent md:rounded-none md:relative md:flex md:flex-row md:overflow-auto">
                         <div class="flex flex-col items-center justify-center w-full h-full mt-12 text-center text-indigo-700 md:text-indigo-200 md:w-2/3 md:mt-0 md:flex-row md:items-center">
-                            <a href="/" class="inline-block px-4 py-2 mx-2 font-medium text-left text-indigo-700 md:text-white md:px-0 lg:mx-3 md:text-center">Home</a>
-                            <a href="/#features" class="inline-block px-0 py-2 mx-2 font-medium text-left md:px-0 hover:text-indigo-800 md:hover:text-white lg:mx-3 md:text-center">Features</a>
+                            <router-link to="/" class="inline-block px-4 py-2 mx-2 font-medium text-left text-indigo-700 md:text-white md:px-0 lg:mx-3 md:text-center">Home</router-link>
+                            <router-link to="/#features" class="inline-block px-0 py-2 mx-2 font-medium text-left md:px-0 hover:text-indigo-800 md:hover:text-white lg:mx-3 md:text-center">Features</router-link>
                             <a href="https://github.com/awesomebible/my-united/discussions/categories/blog" class="inline-block px-0 py-2 mx-2 font-medium text-left md:px-0 hover:text-indigo-800 md:hover:text-white lg:mx-3 md:text-center">Blog</a>
-                            <a href="/kontakt" class="inline-block px-0 py-2 mx-2 font-medium text-left md:px-0 hover:text-indigo-800 md:hover:text-white lg:mx-3 md:text-center">Kontakt</a>
+                            <router-link to="/kontakt" class="inline-block px-0 py-2 mx-2 font-medium text-left md:px-0 hover:text-indigo-800 md:hover:text-white lg:mx-3 md:text-center">Kontakt</router-link>
                         </div>
                     </div>
                 </div>
