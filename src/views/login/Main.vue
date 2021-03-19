f0b725efa6e5db58ee8e59c5907cf3861998c723
Benjamin Hartmann <42138517+benjaminwolkchen@users.noreply.github.com>
2021-03-19 20:21:34 +0100

    Tried bg imgs

--- src/views/login/Main.vue
+++ src/views/login/Main.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+<div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
   <div class="max-w-md w-full space-y-8">
     <div>
       <img class="mx-auto h-12 w-auto" src="/images/login.svg" alt="Schlüssel">
