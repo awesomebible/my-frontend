5c710ac185b5630365bf4438a01c77f2ad7029c1
Benjamin Hartmann <42138517+benjaminwolkchen@users.noreply.github.com>
2021-04-05 17:26:15 +0200

    Move footer into component

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer class="bg-footer-texture">
+    <div class="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
+        <nav class="flex flex-wrap justify-center -mx-5 -my-2">
+            <div class="px-5 py-2">
+                <a href="https://codeberg.org/awesomebible/my-united/issues?type=all&state=open&labels=14798&milestone=0&assignee=0" class="text-base leading-6 text-black-500 hover:text-black-900">
+                    Blog
+                </a>
+            </div>
+            <div class="px-5 py-2">
+                <router-link to="/kontakt" class="text-base leading-6 text-black-500 hover:text-black-900">
+                    Kontakt
+                </router-link>
+            </div>
+            <div class="px-5 py-2">
+                <a href="https://codeberg.org/awesomebible/my-frontend" class="text-base leading-6 text-black-500 hover:text-black-900">
+                    Codeberg ⛰
+                </a>
+            </div>
+        </nav>
+        <p class="mt-8 text-base leading-6 text-center text-black-400" id="">© 2021 awesomeBible</p>
+    </div>
+</footer>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -112,8 +112,7 @@
         </div>
     </div>
 </section>
-
-        <section class="relative py-16 bg-white min-w-screen animation-fade animation-delay">
+<section class="relative py-16 bg-white min-w-screen animation-fade animation-delay">
     <div class="container px-0 px-8 mx-auto sm:px-12 xl:px-5">
         <p class="text-xs font-bold text-left text-pink-500 uppercase sm:mx-6 sm:text-center sm:text-normal sm:font-bold">
             Hast du Fragen? Wir haben Antworten!</p>
@@ -235,26 +234,11 @@
         </div>
     </div>
 </section>
-<footer class="bg-footer-texture">
-    <div class="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
-        <nav class="flex flex-wrap justify-center -mx-5 -my-2">
-            <div class="px-5 py-2">
-                <a href="https://codeberg.org/awesomebible/my-united/issues?type=all&state=open&labels=14798&milestone=0&assignee=0" class="text-base leading-6 text-black-500 hover:text-black-900">
-                    Blog
-                </a>
-            </div>
-            <div class="px-5 py-2">
-                <router-link to="/kontakt" class="text-base leading-6 text-black-500 hover:text-black-900">
-                    Kontakt
-                </router-link>
-            </div>
-            <div class="px-5 py-2">
-                <a href="https://codeberg.org/awesomebible/my-frontend" class="text-base leading-6 text-black-500 hover:text-black-900">
-                    Codeberg ⛰
-                </a>
-            </div>
-        </nav>
-        <p class="mt-8 text-base leading-6 text-center text-black-400" id="">© 2021 awesomeBible</p>
-    </div>
-</footer>
+<FooterComponent/>
 </template>
+<script>
+import FooterComponent from '@/components/FooterComponent.vue';
+export default {
+  components: { FooterComponent },
+}
+</script>
