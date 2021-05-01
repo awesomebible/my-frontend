9be609c0f0217d322c428414ee4d9d6d59fbbe48
Benjamin Hartmann <bendaha@protonmail.com>
2021-05-01 02:17:16 +0200

    About ohne Text hinzugefügt.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,20 @@
 <template>
+    <header class="w-full px-6 mb-12 antialiased bg-white select-none">
+    <div class="mx-auto max-w-7xl">
+        <HomeNavbar />
+        <div class="container max-w-lg px-4 py-32 mx-auto text-left md:max-w-none md:text-center" id="">
+            <h1 class="text-5xl font-extrabold tracking-tight text-left text-gray-900 leading-tightest md:leading-10 md:text-center sm:leading-none md:text-6xl lg:text-7xl"><span class="inline md:block" id="">Wir</span> <span class="relative mt-2 text-transparent md:inline-block bg-clip-text bg-gradient-to-br from-purple-600 to-blue-500" id="">über uns</span></h1>
+            <div class="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg md:text-center lg:text-lg" id=""></div>
+        </div>
+    </div>
+</header>
+<main>
     
+</main>
 </template>
+<script>
+import HomeNavbar from '@/components/HomeNavbar.vue';
+export default {
+  components: { HomeNavbar },
+}
+</script>
