b08955c081c1659ae07eaff1b9c5b3f5056d9e0b
Benjamin H <hbenjamin@tutanota.de>
2021-08-25 18:55:46 +0200

    Dateiauswahl implementiert // Todo onsubmit

--- src/views/app/HomeScreen.vue
+++ src/views/app/HomeScreen.vue
@@ -35,10 +35,10 @@
         <button @click="onPickFile" alt="Anhang hinzufügen" class="inline-flex items-center justify-center px-5 py-3 my-5 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700 mr-5">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
-            </svg>
+            </svg> {{ AttatchedFilename }}
         </button>
         <input id="fileUpload" type="file" ref="fileInput" accept="image/*" @change="onFilePicked" hidden>
-        <button @click="onPickFile" class="inline-flex items-center justify-center px-5 py-3 my-5 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700 mr-5">
+        <button @click="onSubmit" class="inline-flex items-center justify-center px-5 py-3 my-5 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700 mr-5">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
             </svg> Post senden
@@ -51,6 +51,11 @@
 </template>
 <script>
 export default {
+    data() {
+        return {
+            AttatchedFilename: "Noch keine Datei angehängt.",
+        }
+    },
     methods: {
         onPickFile () {
             this.$refs.fileInput.click()
@@ -58,12 +63,16 @@ export default {
         onFilePicked (event) {
             const files = event.target.files
             let filename = files[0].name
+            this.AttatchedFilename = filename
             const fileReader = new FileReader()
             fileReader.addEventListener('load', () => {
                 this.imageUrl = fileReader.result
             })
             fileReader.readAsDataURL(files[0])
             this.image = files[0]
+        },
+        onSubmit() {
+
         }
     }
 }
