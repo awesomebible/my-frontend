d284d5b87b167c9540a7ec48f7af0d0bfc6aa017
Benjamin H <hbenjamin@tutanota.de>
2021-09-18 18:06:25 +0200

    Login Request implementiert.

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -7,20 +7,20 @@
         Melde dich an
       </h2>
     </div>
-    <form class="mt-8 space-y-6" action="#" method="POST">
+    <section class="mt-8 space-y-6">
       <input type="hidden" name="remember" value="true">
       <div class="rounded-md shadow-sm -space-y-px">
         <div>
           <label for="invite-code" class="sr-only">Einladungscode</label>
-          <input id="invite-code" name="invite-code" type="text" autocomplete="text" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Einladungscode">
+          <input v-model="InviteCode" id="invite-code" name="invite-code" type="text" autocomplete="text" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Einladungscode">
         </div>
         <div>
           <label for="email-address" class="sr-only">E-Mailadresse</label>
-          <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="E-Mailadresse">
+          <input v-model="email" id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="E-Mailadresse">
         </div>
         <div>
           <label for="password" class="sr-only">Passwort</label>
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Passwort">
+          <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Passwort">
         </div>
       </div>
 
@@ -38,11 +38,31 @@
         </div>
       </div>
       <div>
-        <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        <button @click="onSubmit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           Anmelden
         </button>
       </div>
-    </form>
+    </section>
   </div>
 </div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+                InviteCode: null,
+                email: null,
+                password: null
+            }
+  },
+  methods: {
+            onSubmit() {
+              this.axios.post("/api/auth", {
+                invitecode: this.InviteCode,
+                email: this.email,
+                password: this.password
+              })    
+            }
+        }
+}
+</script>
