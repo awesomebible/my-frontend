04da19dfe6ff22c6350d5990a55dadfa698b7a05
Benjamin H <hbenjamin@tutanota.de>
2021-09-24 19:35:21 +0200

    Passwort zurücksetzen an API angeschlossen

--- src/views/account/ResetPassword.vue
+++ src/views/account/ResetPassword.vue
@@ -13,20 +13,36 @@
         </router-link>
       </p>
     </div>
-    <form class="mt-8 space-y-6" action="#" method="POST">
+    <section class="mt-8 space-y-6">
       <input type="hidden" name="remember" value="true">
       <div class="rounded-md shadow-sm -space-y-px">
         <div>
           <label for="email-address" class="sr-only">E-Mailadresse</label>
-          <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="E-Mailadresse">
+          <input v-model="email" id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="E-Mailadresse">
         </div>
       </div>
       <div>
-        <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        <button @click="onSubmit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           Neues Passwort anfordern
         </button>
       </div>
-    </form>
+    </section>
   </div>
 </div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+                email: null,
+            }
+  },
+  methods: {
+            onSubmit() {
+              this.axios.post("/api/reset-password", {
+                email: this.email,
+              })    
+            }
+        }
+}
+</script>
