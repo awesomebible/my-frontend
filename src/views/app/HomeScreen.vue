a44c70f94ee5222f6d578d2f75df784270e85e75
Benjamin H <hbenjamin@tutanota.de>
2022-06-16 20:14:42 +0200

    post content with axios

--- src/views/app/HomeScreen.vue
+++ src/views/app/HomeScreen.vue
@@ -20,88 +20,40 @@
         </nav>
     </div>
 </header>
-<main class="text-center mx-60 gab-5 lg:gap-10">
+<main class="text-center mx-60 lg:gap-10 grid grid-cols-5 gap-3">
     <div id="compose-form">
         <h2 class="font-bold text-2xl">Verfasse einen Post</h2>
         <textarea v-model="postContent" class="inline-flex items-center justify-center resize-none appearance-none relative px-3 py-2 mt-4 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" rows="3" placeholder="Was gibt's neues?"></textarea>
-        <br>
-        <div class="inline-flex items-center justify-center px-5 py-5 my-5 mr-5">
-            <label class="block">
-            <span class="sr-only">Lade Bilder hoch</span>
-            <input type="file" class="block w-full text-sm text-gray-500
-                file:mr-4 file:py-2 file:px-4
-                file:rounded-md file:border-0
-                file:text-sm file:font-semibold
-                file:bg-indigo-600 file:text-white
-                hover:file:bg-indigo-700
-            "/>
-            </label>
-        </div>
-        <button @click="onSubmit" class="inline-flex items-center justify-center text-white
+        
+        <button @click="Submit" class="inline-flex text-white
                 mr-4 mt-4 py-2 px-4 rounded-md file:border-0 text-sm font-semibold bg-indigo-600 hover:bg-indigo-700
             ">
             Post senden
         </button>
     </div>
-    <div id="feed" class="">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam labore rem, maiores saepe voluptatum fugit sequi recusandae, a debitis natus corrupti neque quibusdam! Eligendi odit cupiditate id numquam accusantium minima?
+    <div id="feed" class="col-span-4">
+        hallo
     </div>
 </main>
 </template>
 <script>
-import { v4 as uuidv4 } from 'uuid';
-
 export default {
-    data() {
+      data() {
         return {
-            AttatchedFilename: "Noch keine Datei angehängt.",
-            postTitle: null,
-            postContent: null
+        postContent:"Hallo",
+        }
+      },
+    methods: {
+        Submit() {
+        alert(this.postContent)
+        this.axios.post('http://localhost:5000/post/new', {
+            content: this.postContent
+        }).then(function (response) {
+            console.log(response);
+        }).catch(function (error) {
+            console.log(error);
+        });
         }
     },
-    methods: {
-        onPickFile () {
-            this.$refs.fileInput.click()
-        },
-        onFilePicked (event) {
-            const files = event.target.files
-            let filename = files[0].name
-            this.AttatchedFilename = filename
-            const fileReader = new FileReader()
-            fileReader.addEventListener('load', () => {
-                this.imageUrl = fileReader.result
-            })
-            fileReader.readAsDataURL(files[0])
-            this.image = files[0]
-        },
-        onSubmit() {
-            if (!this.filename){
-                    const formData = new FormData();
-                    formData.append('file', this.image);
-                    const headers = { 'Content-Type': 'multipart/form-data' };
-                    this.axios.post('/api/create-file', formData, { headers }).then((res) => {
-                    res.data.files; // binary representation of the file
-                    res.status; // HTTP status
-                })
-                .then(function (response) {
-                    console.log(response.data);
-                    console.log(response.status);
-                    console.log(response.statusText);
-                    console.log(response.headers);
-                    console.log(response.config);
-                });
-            }
-            this.axios.post('/api/create-post', {
-                postTitle: this.postTitle,
-                postContent: this.postContent,
-            })
-        .then(function (response) {
-                console.log(response);
-            })
-        .catch(function (error) {
-                console.log(error);
-            });
-        }
-    }
 }
 </script>
